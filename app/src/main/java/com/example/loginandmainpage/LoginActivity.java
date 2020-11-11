5f91e643bed28cdf3629007955a4cd475f204f38
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-11-11 01:22:49 -0400

    splash, ripper and register activity created.

--- app/src/main/java/com/example/loginandmainpage/LoginActivity.java
+++ app/src/main/java/com/example/loginandmainpage/LoginActivity.java
@@ -1,21 +1,28 @@
 package com.example.loginandmainpage;
-
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView register_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
+        register_text = findViewById(R.id.register_text_id);
+        register_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View text) {
+                Intent register_intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(register_intent);
+            }
+        });
 
     }
 
-    //TODO: make custom cylinder button
-    //TODO: Can I download Century Gothic letters
-    //TODO: Change icon shipping cart
-    //Add an animation to the splash intro
+
 }
--- app/src/main/java/com/example/loginandmainpage/MainActivity.java
+++ app/src/main/java/com/example/loginandmainpage/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.loginandmainpage;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView ecommerce_splash_text;
+    private ImageView cart_splash_welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ecommerce_splash_text = findViewById(R.id.ecommerce_splash_id);
+        cart_splash_welcome = findViewById(R.id.cart_splash_welcome_id);
+
+        Animation up_splash_animation = AnimationUtils.loadAnimation(this, R.anim.move_up);
+        Animation down_splash_animation = AnimationUtils.loadAnimation(this, R.anim.move_down);
+
+        cart_splash_welcome.setAnimation(up_splash_animation);
+        ecommerce_splash_text.setAnimation(down_splash_animation);
 
         final Handler handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(new Runnable() {
@@ -21,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
                startActivity(afterSplayDelay);
                finish();
             }
-        }, 2000);
+        }, 4000);
+
+
     }
 }
+
+//TODO: make custom cylinder button (Done)
+//TODO: Can I download Century Gothic letters
+//TODO: Change icon shipping cart (Done)
+//TODO: Add an loading animation to the splash intro
