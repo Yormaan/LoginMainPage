1a515a3ed90970e1e9bbc7ae561652bf3d16992f
Nikorux <nicole.0126@hotmail.es>
2020-11-11 12:29:48 -0400

    + stuff

--- app/src/main/java/com/pucmm/loginmainpage/RegisterActivity.java
+++ app/src/main/java/com/pucmm/loginmainpage/RegisterActivity.java
@@ -1,14 +1,47 @@
 package com.pucmm.loginmainpage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ImageView userPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        userPicture = (ImageView) findViewById(R.id.LaFoto);
+    }
+
+    public void onclick(View view) {
+        cargarFoto();
+    }
+
+    private void cargarFoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent, "Seleccione la Aplicación"), 10);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            Uri path = data.getData();
+            userPicture.setImageURI(path);
+        }
     }
 }
+
+
+
