20f6a2b2d866f33be8ee6aad611e1fc90dd574fb
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-11-11 14:34:39 -0400

    Taking Main branch changes to master

--- app/src/main/java/com/pucmm/loginmainpage/MainActivity.java
+++ app/src/main/java/com/pucmm/loginmainpage/MainActivity.java
@@ -1,5 +1,10 @@
 package com.pucmm.loginmainpage;
 
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,23 +14,51 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView ecommerce_splash_text;
+    private ImageView cart_splash_welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ecommerce_splash_text = findViewById(R.id.ecommerce_splash_id);
+        cart_splash_welcome = findViewById(R.id.cart_splash_welcome);
 
-        getSupportActionBar().hide();
+        Animation up_splash_animation = AnimationUtils.loadAnimation(this, R.anim.move_up);
+        Animation down_splash_animation = AnimationUtils.loadAnimation(this, R.anim.move_down);
 
-        new Handler().postDelayed(new Runnable() {
+        cart_splash_welcome.setAnimation(up_splash_animation);
+        ecommerce_splash_text.setAnimation(down_splash_animation);
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                Intent afterSplayDelay = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(afterSplayDelay);
                 finish();
             }
-        }, 3000);
+        }, 4000);
 
 
     }
-
 }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        getSupportActionBar().hide();
+//
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        }, 3000);
+//
+//
+//    }
+
