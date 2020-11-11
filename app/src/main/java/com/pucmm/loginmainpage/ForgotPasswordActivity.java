00d3a0d2f7294bd555671569bfd7f16089150731
IronelisHerrera <40542187+IronelisHerrera@users.noreply.github.com>
2020-11-11 17:00:32 -0400

    Navigation between register, forgot pass and login completed

--- app/src/main/java/com/pucmm/loginmainpage/ForgotPasswordActivity.java
+++ app/src/main/java/com/pucmm/loginmainpage/ForgotPasswordActivity.java
@@ -1,15 +1,39 @@
 package com.pucmm.loginmainpage;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    private TextView btn_register;
+    private TextView btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        btn_register = findViewById(R.id.btn_register_fp_id);
+        btn_login = findViewById(R.id.btn_login_fp_id);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPasswordActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
